5274b3fb5b4542d604ace61618ef81ae32648cda
giveMepower94 <Kul.vova2009@yandex.ru>
2024-04-14 03:33:31 +0300

    Написали функции login_user, logout_user.

--- users/views.py
+++ users/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
+from .forms import LoginUserForm
 
 # Create your views here.
 
-def login_user():
-    pass
+def login_user(request):
+    if request.method == 'POST':
+        form = LoginUserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                form.add_error(None, 'Неверное имя пользователя или пароля!')
+    else:
+        form = LoginUserForm()
+    return render(request, 'users/login.html', {'form': form})
 
 
-def logout_user():
-    pass
+def logout_user(request):
+    logout(request)
+    return redirect(reverse('users: login'))
