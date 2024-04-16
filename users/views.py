1667693614979ae16e4703ac1cdd7f9d4dea84c5
giveMepower94 <Kul.vova2009@yandex.ru>
2024-04-16 12:04:24 +0300

    Заменили функцию logout_user на класс LogoutUser

--- users/views.py
+++ users/views.py
@@ -1,23 +1,21 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from .forms import LoginUserForm
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 
 # Create your views here.
 
 class LoginUser(LoginView):
-    form_class = AuthenticationForm
+    form_class = LoginUserForm
     template_name = 'users/login.html'
     extra_context = {'title': 'Авторизация'}
 
     def get_success_url(self):
         return reverse_lazy('index')
 
-def logout_user(request):
-    logout(request)
-    return redirect(reverse('users: login'))
+class LogoutUser(LogoutView):
+    next_page = reverse_lazy('users:login')
 
 
 def signup_user(request):
