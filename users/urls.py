47f2f9494aeb8cdd52f99731bce4c8ae3533c4a9
giveMepower94 <Kul.vova2009@yandex.ru>
2024-04-30 20:30:34 +0300

    Настроили сброс пароля, нужно устранить ошибку в маршрутизации.

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 
 app_name = 'users'  # Пространство имен для приложения
@@ -14,4 +16,22 @@ urlpatterns = [
     path('password_change/', views.UserPasswordChange.as_view(), name='password_change'),
     path('password_change_done/', views.UserPasswordChangeDone.as_view(), name='password_change_done'),
     path('profile_cards/', views.UserCardsView.as_view(), name='profile_cards'),
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='users/password_reset_form.html',
+             email_template_name='users/password_reset_email.html',
+             success_url = reverse_lazy('password_reset_done')),
+         name='password_reset'),
+    path('password_reset_done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name = 'users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('reset_done/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name = 'users/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
