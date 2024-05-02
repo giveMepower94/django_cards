68e9c85ca35634c6c78a7fcef8188ccb907edccd
giveMepower94 <Kul.vova2009@yandex.ru>
2024-05-02 14:48:00 +0300

    Устранили ошибку в функции сброса пароля

--- users/urls.py
+++ users/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
          auth_views.PasswordResetView.as_view(
              template_name='users/password_reset_form.html',
              email_template_name='users/password_reset_email.html',
-             success_url = reverse_lazy('password_reset_done')),
+             success_url = reverse_lazy('users:password_reset_done')),
          name='password_reset'),
     path('password_reset_done/',
          auth_views.PasswordResetDoneView.as_view(
@@ -28,7 +28,8 @@ urlpatterns = [
          name='password_reset_done'),
     path('reset/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(
-             template_name = 'users/password_reset_confirm.html'),
+             template_name = 'users/password_reset_confirm.html',
+             success_url = reverse_lazy('users:password_reset_complete')),
          name='password_reset_confirm'),
     path('reset_done/',
          auth_views.PasswordResetCompleteView.as_view(
